88cd0e4d68f7d141b3d7131be68747821e09e5ea
ana lópez laguna <you@example.com>
2021-04-18 00:27:12 +0200

    update responsive header with menu hamburguer

--- src/js/main.js
+++ src/js/main.js
@@ -115,3 +115,13 @@ hookAnimations();
 
 
 
+// MENU HAMBURGUESA
+var menu = document.querySelector('.hamburger');
+
+function toggleMenu(event) {
+    this.classList.toggle('is-active');
+    document.querySelector('.menuppal').classList.toggle("is_active");
+    event.preventDefault();
+}
+
+menu.addEventListener('click', toggleMenu, false);
